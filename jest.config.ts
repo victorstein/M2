548e89754c50e4cc9a1dccf95298f9262a17bea1
alfonso gomez <stein.hakase.vs@gmail.com>
2020-10-25 03:15:38 -0600

    🔧 Created Jest configuration file

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  displayName: {
+    name: 'M2',
+    color: 'cyan'
+  },
+  modulePathIgnorePatterns: [
+    '_transpiled'
+  ],
+  silent: false,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!**/node_modules/**"
+  ],
+  forceExit: true
+};
