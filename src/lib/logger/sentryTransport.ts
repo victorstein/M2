fe8911875d67a613f8d6ecf57a6731845677bf97
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-05-17 00:42:09 -0600

    🔥 Removed Sentry Loader and tests in favor of SentryTransport

---
+++ src/lib/logger/sentryTransport.ts
@@ -0,0 +1,44 @@
+import { Service } from 'typedi'
+import TransportStream from 'winston-transport'
+import * as Sentry from '@sentry/node'
+import config from 'config'
+import { LogLevel } from 'services/types/loggerServiceTypes'
+
+@Service()
+class SentryTransport extends TransportStream {
+  private readonly sentry: typeof Sentry
+
+  constructor (dsn?: string, serverName?: string) {
+    super()
+    dsn = dsn ?? config.SENTRY_DSN
+    serverName = serverName ?? config.SENTRY_SERVER_NAME
+    this.sentry = Sentry
+    this.initializeSentry(dsn, serverName)
+  }
+
+  private initializeSentry (dsn: string, serverName: string): void {
+    try {
+      this.sentry.init({ dsn, serverName, environment: config.ENV })
+      console.log('Sentry Initialized successfully ✅')
+    } catch (e) {
+      console.log('Error Initializing Sentry: 🚨 ->', e.message)
+    }
+  }
+
+  public log (info: any, callback: () => void): void {
+    setImmediate(() => {
+      this.emit('logged', info)
+    })
+
+    if (info[Symbol.for('level')] === LogLevel.ERROR) {
+      const meta: any[] = info[Symbol.for('splat')]
+      if (meta !== undefined) {
+        this.sentry.setContext('Additional info', Object.assign({}, ...meta))
+      }
+      this.sentry.captureException(info)
+    }
+    return callback()
+  }
+}
+
+export default SentryTransport
