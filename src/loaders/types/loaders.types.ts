127f5d01fd233a14fd19d6213d1933ebe85a589b
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-05-30 19:22:41 -0600

    🚚 Renamed types in loaders ✨ Added containers symbols ✨ Created loader interface

--- src/loaders/types/loaders.types.ts
+++ src/loaders/types/loaders.types.ts
@@ -1,8 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 import { Request, Response } from 'express'
+import { Token } from 'typedi'
+
+export interface ILoader {
+  start: () => Promise<void> | void
+}
 
 export interface Context {
   prisma: PrismaClient
   req: Request
   res: Response
+}
+
+export const CONTAINER = {
+  LOGGER: new Token<string>('logger')
 }
