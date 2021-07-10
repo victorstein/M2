c0c81278d5cbabeaca93e4c15bf9a4b9fcb53b90
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-10 15:01:42 -0600

    ✨ Created base class to extend models and provide createdBy and lastUpdated by functionality

---
+++ src/db/models/base.ts
@@ -0,0 +1,56 @@
+import { DocumentType, post, pre, prop, Ref } from '@typegoose/typegoose'
+import { injectable } from 'inversify'
+import containerLoader from 'loaders/containerLoader'
+import { ContainerTypes } from 'loaders/types/loadersTypes'
+import { Field, ObjectType } from 'type-graphql'
+import User from './user'
+
+@post<Base>('findOneAndUpdate', function (doc: any, next) {
+  const { lastErrorObject, value } = doc
+  const base: DocumentType<Base> = value
+  const userId: Ref<User> = containerLoader.get(ContainerTypes.USER_CONTEXT)
+
+  const isNew = !lastErrorObject.updatedExisting
+
+  if (isNew && userId) {
+    // Set the createdby value
+    base.set({ createdBy: userId })
+    return base.save({ validateBeforeSave: false })
+      .then(() => next())
+  }
+
+  if (!isNew && userId) {
+    // Set the lastUpdatedBy value
+    base.set({ lastUpdatedBy: userId })
+    return base.save({ validateBeforeSave: false })
+      .then(() => next())
+  }
+
+  return next()
+})
+
+@pre<Base>('validate', function (next) {
+  const userId: Ref<User> = containerLoader.get(ContainerTypes.USER_CONTEXT)
+  
+  if (this.isNew && userId) {
+    this.createdBy = userId
+    return next()
+  }
+
+  this.lastUpdatedBy = userId
+  return next()
+})
+
+@ObjectType()
+@injectable()
+class Base {
+  @Field(() => String, { nullable: true })
+  @prop({ ref: () => User, required: false })
+  createdBy: Ref<User>
+
+  @Field(() => String, { nullable: true })
+  @prop({ ref: () => User, required: false })
+  lastUpdatedBy: Ref<User>
+}
+
+export default Base
