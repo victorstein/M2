9abf09377bc22b5b453c2fa4362342b3fa6df534
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-10 15:05:33 -0600

    ✨ Created admin and role seeder files

--- src/db/seeder/adminSeed.ts
+++ src/db/seeder/adminSeed.ts
@@ -1,38 +1,66 @@
 import { mongoose } from '@typegoose/typegoose'
 import config from 'config'
 import { Roles } from 'db/models/types/modelsTypes'
-import User from 'db/models/user'
 import { inject, injectable } from 'inversify'
 import ContainerLoader from 'loaders/containerLoader'
 import MongoLoader from 'loaders/mongoLoader'
 import { ContainerTypes } from 'loaders/types/loadersTypes'
+import { RoleService } from 'services/roleService'
+import { UserService } from 'services/userService'
 import { Logger } from 'winston'
-import { ConnectionState, Seeder } from './types/seederTypes'
+import { RoleSeeder } from './roleSeed'
+import { ConnectionState, ISeeder } from './types/seederTypes'
 
 @injectable()
-export class AdminSeed implements Seeder {
-  @inject(ContainerTypes.USER_MODEL)
-  user: User
-
+export class AdminSeed implements ISeeder {
   @inject(ContainerTypes.LOGGER)
   logger: Logger
+
+  @inject(ContainerTypes.USER_SERVICE)
+  userService: UserService
+
+  @inject(ContainerTypes.ROLE_SERVICE)
+  roleService: RoleService
+
+  @inject(ContainerTypes.ROLE_SEEDER)
+  roleSeeder: RoleSeeder
 
   async seed (): Promise<void> {
     try {
       this.logger.verbose('Looking for admin user')
-      const admin = await this.user.model.findOne({ 'role.name': Roles.USER })
+      const admin = await this.userService.findOneByRole(Roles.ADMIN)
 
       if (admin === null) {
-        this.logger.verbose('Admin user was not found. Creating admin user.')
-        await this.user.model.create({
+        this.logger.verbose('Admin user was not found.')
+
+        this.logger.verbose('Check if admin email on .env')
+        if (config.ADMIN_EMAIL === null) throw new Error('Database seed error. ADMIN_EMAIL not set in .env file.')
+
+        this.logger.verbose('Checking if ADMIN role exists...')
+        let adminRole = await this.roleService.findOneByParam({ name: Roles.ADMIN })
+
+        if (!adminRole) {
+          this.logger.verbose('Admin role not found. Creating Base roles...')
+          await this.roleSeeder.seed()
+        }
+
+        adminRole = await this.roleService.findOneByParam({ name: Roles.ADMIN })
+
+        const password = this.userService.generateTemporaryPassword()
+        const hashedPassword = await this.userService.hashPassword(password)
+
+        this.logger.verbose('Add user to DB')
+        await this.userService.create({
           firstName: 'admin',
           lastName: 'admin',
-          email: config.ADMIN_EMAIL
+          email: config.ADMIN_EMAIL,
+          password: hashedPassword,
+          role: adminRole?._id
         })
         return
       }
 
-      this.logger.verbose(`There's an existing admin user in the DB. New admin not created. Please consult with ${admin.email}`)
+      this.logger.verbose('There\'s an existing admin user in the DB. New admin not created.')
     } catch (e) {
       this.logger.error(`There was an error seeding the DB: ${e.message as string}`)
       throw new Error(e.message)
