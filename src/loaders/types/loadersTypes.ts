9cee0fc2e98d9db9c6182c90903f82782ae94f2c
Alfonso Gomez <stein.hakase.vs@gmail.com>
2021-07-12 01:06:10 -0600

    ✨ Created subscriber loader 🏷️ Added definition to loaderTypes

--- src/loaders/types/loadersTypes.ts
+++ src/loaders/types/loadersTypes.ts
@@ -1,5 +1,4 @@
-import { ApolloServer } from 'apollo-server-express'
-import { Application, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { Logger } from 'winston'
 
 export interface Context {
@@ -13,15 +12,19 @@ export const ContainerTypes = {
   NOTFOUND: Symbol.for('NOT_FOUND'),
   ROLE_SEEDER: Symbol.for('ROLE_SEEDER'),
   DISPATCHER: Symbol.for('DISPATCHER'),
+  EMAIL_TRANSPORT: Symbol.for('EMAIL_TRANSPORT'),
   // Loader types
   APOLLO_LOADER: Symbol.for('APOLLO_LOADER'),
   EXPRESS_LOADER: Symbol.for('EXPRESS_LOADER'),
   MONGO_LOADER: Symbol.for('MONGO_LOADER'),
+  EMAIL_LOADER: Symbol.for('EMAIL_LOADER'),
+  SUBSCRIBER_LOADER: Symbol.for('SUBSCRIBER_LOADER'),
   LOADERS: Symbol.for('LOADERS'),
   // Service types
   USER_SERVICE: Symbol.for('USER_SERVICE'),
   ROLE_SERVICE: Symbol.for('ROLE_SERVICE'),
   PERMISSION_SERVICE: Symbol.for('PERMISSION_SERVICE'),
+  EMAIL_SERVICE: Symbol.for('EMAIL_SERVICE'),
   // Model types
   USER_MODEL: Symbol.for('USER_MODEL'),
   ROLE_MODEL: Symbol.for('ROLE_MODEL'),
@@ -30,19 +33,7 @@ export const ContainerTypes = {
   USER_CONTEXT: Symbol.for('USER_CONTEXT')
 }
 
-export enum LoaderTypes {
-  'APOLLO' = 'Apollo',
-  'EXPRESS' = 'Express',
-  'VOID' = 'void'
-}
-
-export interface Loaders {
-  Apollo: () => Promise<ApolloServer>
-  Express: () => Application
-  void: () => void | Promise<void>
-}
-
-export interface ILoader<T extends keyof Loaders> {
+export interface ILoader<T> {
   logger: Logger
-  start: Loaders[T]
+  start: () => T
 }
